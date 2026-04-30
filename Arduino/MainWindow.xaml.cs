22ba62232218b679d6aebf1fa9527a10dfa4324b
k1wicek <kiiwicek@gmail.com>
2026-04-30 18:43:42 +0200

    Light/Dark mode

--- Arduino/MainWindow.xaml.cs
+++ Arduino/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Arduino
 {
     public partial class MainWindow : Window
     {
         Dictionary<string, (string title, string content)> articles;
+        bool LightMode = true;
 
         public MainWindow()
         {
@@ -128,6 +129,32 @@ namespace Arduino
                     ArticleTitle.Text = article.title;
                     ArticleContent.Text = article.content;
                 }
+            }
+        }
+
+        private void ModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (LightMode)
+            {
+                this.Resources["Background"] = new SolidColorBrush(Color.FromRgb(29, 31, 46));
+                this.Resources["Foreground"] = new SolidColorBrush(Color.FromRgb(36, 39, 61));
+                this.Resources["Text"] = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.Resources["Text2"] = new SolidColorBrush(Color.FromRgb(155, 155, 155));
+                ModeTextBlock.Text = "Světlý režim";
+                button.Content = "☀︎";
+                LightMode = false;
+            }
+            else
+            {
+                this.Resources["Background"] = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.Resources["Foreground"] = new SolidColorBrush(Color.FromRgb(233, 227, 255));
+                this.Resources["Text"] = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                this.Resources["Text2"] = new SolidColorBrush(Color.FromRgb(135, 135, 135));
+                ModeTextBlock.Text = "Tmavý režim";
+                button.Content = "⏾";
+                LightMode = true;
             }
         }
     }
